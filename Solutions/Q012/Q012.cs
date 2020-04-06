f5fce9cbcb06ef3d717e184a1392e4a0e2a3255b
Buket <bktmzrak@gmail.com>
2020-04-06 17:21:32 +0300

    Added solution 12 for C#

---
+++ Solutions/Q012/Q012.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProjectEuler.Solutions.Q012
+{
+    class Q012
+    {
+        static void Main (string[] args)
+        {
+            var j = 1;
+            var kosul = true;
+            
+            while (kosul)
+            {
+                var deger = j * (j + 1) / 2;
+                var sayac = 0;
+                for (var i = 1; i <= Math.Sqrt(deger); i++)
+                {
+                    if (deger % i == 0)
+                    {
+                        sayac+=2;
+                    }
+
+                }                   
+                if (sayac >= 500)
+                {
+                    Console.WriteLine(deger);
+                    Console.ReadLine();
+                    kosul = false;
+                }
+                j++;
+            }
+        }
+           
+    }
+    
+}
