1adf18da929bcddd02be17982bfa626d6d8dff09
Buket <bktmzrak@gmail.com>
2020-04-14 15:31:26 +0300

    Added solution 14 for C#

---
+++ Solutions/Q014/Q014.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ProjectEuler.Solutions.Q014
+{
+    class Q014
+    {
+        static void Main(string[] args)
+        {
+            var item = 0;
+            var sequence = 0;
+            for (var a = 2; a <= 1000000; a++)
+            {
+                var chain = 1;
+                long number = a; //attention!
+                while (number != 1)
+                {
+                    if (number % 2 == 0) //even
+                    {
+                        number = number / 2;
+                    }
+                    else //odd
+                    {
+                        number = 3 * number + 1;
+
+                    }
+                    chain++;
+
+                }
+
+                if (chain > item)
+                {
+                    item = chain;
+                    sequence = a;
+
+                }
+
+            }
+            Console.WriteLine("{0} {1}", sequence, item);
+            Console.ReadLine();
+
+        }
+    }
+}
