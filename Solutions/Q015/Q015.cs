0eab491411b4a6aac8eca7735b74f50f98a0b67b
Buket <bktmzrak@gmail.com>
2020-04-16 15:24:49 +0300

    Added solution 15 for C#

---
+++ Solutions/Q015/Q015.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProjectEuler.Solutions.Q015
+{
+    class Q015
+    {
+        static void Main(string[] args)
+        {
+            long[,] sequence = new long[21, 21];
+            long item=0;
+
+            for (long i = 0; i <= 19; i++)
+            {
+                sequence[i+1, 0] = 1;
+                for (long j = 0; j <= 20; j++)
+                {
+                    if (j < 20)
+                    {
+                        sequence[0, j+1] = 1;                  
+                        item = sequence[i, j+1] + sequence[i+1, j];
+                        sequence[i+1, j+1] = item;
+                    }
+                }
+               
+            }
+            Console.WriteLine(item);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
