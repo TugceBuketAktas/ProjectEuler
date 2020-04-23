800dc4e5fa610dead2221ea131f95644d3cdc403
Buket <bktmzrak@gmail.com>
2020-04-24 00:27:04 +0300

    Added solution 16 for C#

---
+++ Solutions/Q016/Q016.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace ProjectEuler.Solutions.Q016
+{
+    class Q016
+    {
+        static void Main(string[] args) {
+
+            BigInteger numericBase = 2;
+            BigInteger num = 0, sum = 0;
+            func();
+
+            void func()
+            {
+                for (int ctr = 0; ctr <= 1000; ctr++)
+                {
+                    num = BigInteger.Pow(numericBase, ctr);
+                }
+                while (num != 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                Console.WriteLine(sum);
+                Console.ReadLine();
+            }
+        }        
+    }   
+}
